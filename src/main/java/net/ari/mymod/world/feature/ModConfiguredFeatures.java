06c93f9c03a4665fa3bebf90a75c8efa193b8c44
Logan <a;dbaiwd@gmail>
2023-05-22 09:54:35 -0700

    Ore Generation almost completed

--- src/main/java/net/ari/mymod/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/ari/mymod/world/feature/ModConfiguredFeatures.java
@@ -7,9 +7,11 @@ import net.ari.mymod.block.ModBlocks;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 
@@ -19,6 +21,10 @@ public class ModConfiguredFeatures {
     public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_POTASSIUM_ORES = Suppliers.memoize(() -> List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.POTASSIUM_ORE.get().defaultBlockState()),
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_POTASSIUM_ORE.get().defaultBlockState())));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> POTASSIUM_ORE = CONFIGURED_FEATURES.register("potassium_ore",
+            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_POTASSIUM_ORES.get(), 7)));
+
     public static void register(IEventBus eventbus) {
         CONFIGURED_FEATURES.register(eventbus);
     }
---
+++ src/main/java/net/ari/mymod/world/feature/ModPlacedFeatures.java
@@ -0,0 +1,38 @@
+package net.ari.mymod.world.feature;
+
+import net.ari.mymod.MyMod;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
+
+public class ModPlacedFeatures {
+    public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
+            DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, MyMod.MOD_ID);
+
+    public static final RegistryObject<PlacedFeature> POTASSIUM_ORE = PLACED_FEATURES.register("potassium_ore_placed",
+            () -> new PlacedFeature(ModConfiguredFeatures.POTASSIUM_ORE.getHolder().get(),
+                    commonOrePlacement(30, // VeinsPerChunk
+                            HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-64), VerticalAnchor.aboveBottom(144)))));
+
+    private static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
+        return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
+    }
+
+    private static List<PlacementModifier> commonOrePlacement(int p_195344_, PlacementModifier p_195345_) {
+        return orePlacement(CountPlacement.of(p_195344_), p_195345_);
+    }
+
+    private static List<PlacementModifier> rareOrePlacement(int p_195350_, PlacementModifier p_195351_) {
+        return orePlacement(RarityFilter.onAverageOnceEvery(p_195350_), p_195351_);
+    }
+
+    public static void register(IEventBus eventBus){
+        PLACED_FEATURES.register(eventBus);
+    }
+}
