7211e23e4acb0a0570251fc4c5c6d5fa4e4db58c
agrace8513 <agrace8513@student.pps.net>
2023-06-02 09:58:11 -0700

    Started Banana Trees

--- src/main/java/net/ari/mymod/block/ModBlocks.java
+++ src/main/java/net/ari/mymod/block/ModBlocks.java
@@ -2,15 +2,19 @@ package net.ari.mymod.block;
 
 //import jdk.incubator.vector.VectorOperators;
 import net.ari.mymod.MyMod;
+import net.ari.mymod.base.ModFlammableRotatedPillarBlock;
 import net.ari.mymod.item.ModCreativeModeTab;
 import net.ari.mymod.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -24,9 +28,9 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, MyMod.MOD_ID);
 
     public static final RegistryObject<Block> BLOCK_OF_BANANA = registerBlock("block_of_banana",
-            () -> new Block(BlockBehaviour
+            () -> new IceBlock(BlockBehaviour
                     .Properties
-                    .of(Material.CACTUS)
+                    .of(Material.ICE)
                     .strength(1.4f)), ModCreativeModeTab.MYMOD_TAB);
 
     public static final RegistryObject<Block> POTASSIUM_ORE = registerBlock("potassium_ore",
@@ -43,6 +47,81 @@ public class ModBlocks {
                     .strength(4f)
                     .requiresCorrectToolForDrops(),
                     UniformInt.of(1, 3)), ModCreativeModeTab.MYMOD_TAB);
+
+    //BANANA TREE
+    public static final RegistryObject<Block> BANANA_LOG = registerBlock("banana_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour
+                    .Properties
+                    .copy(Blocks.OAK_LOG)
+                    .requiresCorrectToolForDrops()),
+                     ModCreativeModeTab.MYMOD_TAB);
+    public static final RegistryObject<Block> BANANA_WOOD = registerBlock("banana_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour
+                    .Properties
+                    .copy(Blocks.OAK_WOOD)
+                    .requiresCorrectToolForDrops()),
+            ModCreativeModeTab.MYMOD_TAB);
+    public static final RegistryObject<Block> STRIPPED_BANANA_LOG = registerBlock("stripped_banana_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour
+                    .Properties
+                    .copy(Blocks.STRIPPED_OAK_LOG)
+                    .requiresCorrectToolForDrops()),
+            ModCreativeModeTab.MYMOD_TAB);
+    public static final RegistryObject<Block> STRIPPED_BANANA_WOOD = registerBlock("stripped_banana_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour
+                    .Properties
+                    .copy(Blocks.STRIPPED_OAK_WOOD)
+                    .requiresCorrectToolForDrops()),
+            ModCreativeModeTab.MYMOD_TAB);
+
+    public static final RegistryObject<Block> BANANA_PLANKS = registerBlock("banana_planks",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour
+                    .Properties
+                    .copy(Blocks.OAK_PLANKS)
+                    .requiresCorrectToolForDrops()) {
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 5;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 20;
+                }
+            },
+            ModCreativeModeTab.MYMOD_TAB);
+    public static final RegistryObject<Block> BANANA_LEAVES = registerBlock("banana_leaves",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour
+                    .Properties
+                    .copy(Blocks.OAK_LEAVES)
+                    .requiresCorrectToolForDrops()) {
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 30;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 60;
+                }
+            },
+            ModCreativeModeTab.MYMOD_TAB);
+
+//    public static final RegistryObject<Block> BANANA_SAPLING = registerBlock("banana_log",
+//            () -> new SaplingBlock( ,BlockBehaviour
+//                    .Properties
+//                    .copy(Blocks.OAK_SAPLING)),
+//            ModCreativeModeTab.MYMOD_TAB);
 
 
 
